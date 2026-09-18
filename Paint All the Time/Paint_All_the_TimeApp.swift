4c0e3fa5e40379f9cd8c8e907121b69cd40a3a50
denis-kolchev <deniskolchev2001@gmail.com>
2026-09-18 19:31:23 +0400

    Initial Commit

---
+++ Paint All the Time/Paint_All_the_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Paint_All_the_TimeApp.swift
+//  Paint All the Time
+//
+//  Created by Denis Kolchev on 18.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct Paint_All_the_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
